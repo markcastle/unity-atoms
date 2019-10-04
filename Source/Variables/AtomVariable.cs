f73cbfff796395fb2d522a2f7f291dd4da8d21db
Mark Castle <mark.castle@captivereality.com>
2019-10-04 14:05:47 +0100

    Added: ResetValue for Variables

--- Source/Variables/AtomVariable.cs
+++ Source/Variables/AtomVariable.cs
@@ -33,6 +33,10 @@ namespace UnityAtoms
             Changed.Raise(Value);
         }
 
+        public void ResetValue()
+        {
+            SetValue(_initialValue);
+        }
         public bool SetValue(T newValue)
         {
             if (!AreEqual(_value, newValue))
